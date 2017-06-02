1ea8f65bd0fdc60e20ba4b142478aebec17fc0f9
alswch <alswchung@gmail.com>
2017-06-02 15:34:08 -0400

    added function: modal box disappers when click window

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var modal = document.getElementById('myModal');
 
-setTimeout (function() {modal.style.display = "block"}, 300);
+setTimeout (function() {modal.style.display = "block"}, 3000);
 
 var close = document.getElementById('close');
 close.addEventListener("click", closeClick);
 
 function closeClick() {
   modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
