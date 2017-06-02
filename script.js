c89e31f4be728a0aa730adedf4dcfa55f3c7b8f4
alswch <alswchung@gmail.com>
2017-06-02 12:49:38 -0400

    added x button. modal closes

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var modal = document.getElementById('myModal');
 
 setTimeout (function() {
-  modal.style.display = "block";}, 3000);
+  modal.style.display = "block"}, 300);
+
+var close = document.getElementById('close');
+close.addEventListener("click", closeClick);
+
+function closeClick() {
+  modal.style.display = "none";
+}
